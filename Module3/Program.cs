e3d8e08379034cc168847517039325b49998436e
oleeriksen <oe@easv.dk>
2023-02-09 21:44:21 +0100

    module3 updated

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -4,6 +4,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        
+    }
+
+    
+
+    private static void printArray(int[] a)
+    {
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.Write(a[i]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
     }
 }
 
