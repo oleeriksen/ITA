b6678f2d41cefb25f270c8079c1c8f4e1d76df6a
oleeriksen <oe@easv.dk>
2023-02-13 21:55:37 +0100

    module4

---
+++ Module4/Person2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Module4
+{
+	public class Person2
+	{
+        // State
+        public string Name { get; set; } = String.Empty;
+        public string Address { get; set; } = String.Empty;
+        public DateOnly Birthday { get; set; }
+        int Height { get; set; }
+        double Weight { get; set; }
+
+        //Methods
+        public bool IsTeenager() { return true; }
+    }
+}
+
---
+++ Module4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Module4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Person peter = new Person();
+
+        peter.name = "Peter";
+        peter.address = "byvej 123, 9876 Andeby";
+        peter.birthday = new DateOnly(1999, 7, 17);
+
+        if (peter.IsTeenager())
+        { }
+
+        Person kurt = new Person("kurt", "lærkevej 12, 8000 aarhus C", 172, 69);
+
+        Person2 egon = new Person2
+        {
+            Name = "Egon",
+            Address = "tremmely 1",
+            Birthday = DateOnly.FromDateTime(DateTime.Now)
+        };
+    }
+}
+
